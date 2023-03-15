afdea847038440918f47f8d376147be7f9699882
Olayiwola Abdulrahmon <93688539+Omo-ola@users.noreply.github.com>
2023-03-15 19:59:14 +0100

    mended

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+const cFun = 'C is fun';
+const pyCool = "Python is cool";
+const jaAmaze = "JavaScript is amazing"
+console.log(cFun);
+console.log(pyCool);
+console.log(jaAmaze);
