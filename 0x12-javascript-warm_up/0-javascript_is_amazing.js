d9938d590b96aa6c98e8464e8087d77492b4acad
Olayiwola Abdulrahmon <93688539+Omo-ola@users.noreply.github.com>
2023-03-15 20:08:16 +0100

    change

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const log = "JavaScript is amazing"
-console.log(log);
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const cFun = 'C is fun';
-const pyCool = "Python is cool";
-const jaAmaze = "JavaScript is amazing"
-console.log(cFun);
-console.log(pyCool);
-console.log(jaAmaze);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
